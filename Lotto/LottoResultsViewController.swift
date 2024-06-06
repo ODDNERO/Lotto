17dade2a49615d4c9ba137b5eb23b03a106fa49d
NERO <mxzxro@gmail.com>
2024-06-07 03:28:43 +0900

    🔧 SceneDelegate Window RootVC 설정

---
+++ Lotto/LottoResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LottoResultsViewController.swift
+//  Lotto
+//
+//  Created by NERO on 6/6/24.
+//
+
+import UIKit
+
+class LottoResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
