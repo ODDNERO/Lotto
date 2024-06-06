f5e394f37d5a5f878e0a25ff01303c47f48c317e
NERO <mxzxro@gmail.com>
2024-06-07 03:57:23 +0900

    ✨ [LottoResultsVC] 기본 틀 구성, Title 지정

--- Lotto/LottoResultsViewController.swift
+++ Lotto/LottoResultsViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class LottoResultsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        configureHierarchy()
+        configureData()
+        configureLayout()
+        configureUI()
+        
+        settingNavigation()
+    }
+}
+
+//MARK: - Configure
+extension LottoResultsViewController {
+    func configureHierarchy() {
+        
+    }
+    
+    func configureData() {
+        
+    }
+}
+
+//MARK: - Configure UI
+extension LottoResultsViewController {
+    func configureLayout() {
+        
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+}
+
+//MARK: - Switching View
+extension LottoResultsViewController {
+    func settingNavigation() {
+        navigationItem.title = "🎁 당첨 번호 조회"
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor(resource: .main)
+        ]
     }
 }
