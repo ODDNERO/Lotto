6242d66532264d3ebf98e04088239b38df11443b
NERO <mxzxro@gmail.com>
2024-06-07 08:17:45 +0900

    ✨ [LottoResultsVC] Property 생성

--- Lotto/LottoResultsViewController.swift
+++ Lotto/LottoResultsViewController.swift
@@ -11,6 +11,57 @@ import SnapKit
 
 class LottoResultsViewController: UIViewController {
     
+    let roundTextField = UITextField()
+    let roundPickerView = UIPickerView()
+    
+    let lottoDateLabel = UILabel()
+    
+    let lottoRoundLabel = UILabel()
+    let resultLabel = UILabel()
+    let resultStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        //추가 설정 필요
+        return stackView
+    }()
+    
+    lazy var lottoNumbers = [lottoNo1Label, lottoNo2Label, lottoNo3Label, lottoNo4Label, lottoNo5Label, lottoNo6Label, plusLabel, lottoBonusLabel]
+    let lottoNo1Label = UILabel()
+    let lottoNo2Label = UILabel()
+    let lottoNo3Label = UILabel()
+    let lottoNo4Label = UILabel()
+    let lottoNo5Label = UILabel()
+    let lottoNo6Label = UILabel()
+    let plusLabel = UILabel()
+    let lottoBonusLabel = UILabel()
+    let lottoNumberStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    let firstPrizeMoneyTitleLabel = UILabel()
+    let firstPrizeMoneyLabel = UILabel()
+    let prizeMoneyStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        //추가 설정 필요
+        return stackView
+    }()
+    
+    let firstWinnerCountTitleLabel = UILabel()
+    let firstWinnerCountLabel = UILabel()
+    let winnerCountStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        //추가 설정 필요
+        return stackView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
